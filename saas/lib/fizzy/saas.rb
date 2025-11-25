e6a015b931c34bc349bb49bea19ad15eaeaf12a9
Jorge Manrubia <jorge@hey.com>
2025-11-25 13:09:49 +0100

    Prefer path via the engine

--- saas/lib/fizzy/saas.rb
+++ saas/lib/fizzy/saas.rb
@@ -4,9 +4,9 @@ require "fizzy/saas/engine"
 module Fizzy
   module Saas
     def self.append_test_paths
-      saas_test_dir = "#{Gem::Specification.find_by_name("fizzy-saas").gem_dir}/test"
-      ENV["DEFAULT_TEST"] = "{#{saas_test_dir},test}/**/*_test.rb"
-      ENV["DEFAULT_TEST_EXCLUDE"] = "{#{saas_test_dir},test}/{system,dummy,fixtures}/**/*_test.rb"
+      engine_test_path = Engine.root.join("test")
+      ENV["DEFAULT_TEST"] = "{#{engine_test_path},test}/**/*_test.rb"
+      ENV["DEFAULT_TEST_EXCLUDE"] = "{#{engine_test_path},test}/{system,dummy,fixtures}/**/*_test.rb"
     end
   end
 end
